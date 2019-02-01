215d6477c2f2270d97944e4b0a31bd7254ed8e6b
jake <jake@mythical.fish>
2019-02-01 13:16:32 -0700

    🚀 Lez do dis!

---
+++ app/src/main/java/com/mythicalfish/wonderfulwake/MainActivity.java
@@ -0,0 +1,27 @@
+package com.mythicalfish.wonderfulwake;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+
+        FloatingActionButton newAlarmBtn = findViewById(R.id.newAlarm);
+        newAlarmBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent newAlarm = new Intent(getApplicationContext(), NewAlarm.class);
+                startActivity(newAlarm);
+            }
+        });
+    }
+}
