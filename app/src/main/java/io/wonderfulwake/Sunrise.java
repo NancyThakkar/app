b20c6a7b075c5c8b3aee55be0c59612dd79c1f4c
jake <jake@mythical.fish>
2019-03-30 18:02:39 -0600

    Various fixes, improved video

--- app/src/main/java/io/wonderfulwake/Sunrise.java
+++ app/src/main/java/io/wonderfulwake/Sunrise.java
@@ -19,48 +19,67 @@ import java.util.GregorianCalendar;
 
 public class Sunrise extends AppCompatActivity {
 
+    private MediaPlayer audioPlayer = MediaPlayer.create(this, R.raw.rainforest_1a);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Wake the phone
         WakeLocker.acquire(this);
-        this.getWindow().setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN |
-            WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
-            WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
-            WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN |
-            WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
-            WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
-            WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
-        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        // Make fullscreen, landscape, etc.
+        setScreenOptions();
+
+        // Set the view
         setContentView(R.layout.sunrise);
 
-        // Audio: birdsong
-        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.rainforest_1a);
-        mediaPlayer.start();
+        // Play audio: birdsong
+        audioPlayer.start();
 
-        // Video: sunrise
-        VideoView video = findViewById(R.id.videoView);
-        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.video_1a);
-        video.setVideoURI(uri);
-        video.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+        // Play video: sunrise
+        VideoView videoPlayer = findViewById(R.id.videoView);
+        Uri videoURI = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.video_1a);
+        videoPlayer.setVideoURI(videoURI);
+        videoPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
                 mp.setLooping(true);
             }
         });
-        video.start();
+        videoPlayer.start();
 
 
-        // Dismiss
+        // Dismiss button
         Button dismissBtn = findViewById(R.id.dismissBtn);
         dismissBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mediaPlayer.stop();
+                audioPlayer.release();
                 finish();
             }
         });
     }
 
+    private void setScreenOptions() {
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        this.getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN |
+                        WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN |
+                        WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON );
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_FULLSCREEN );
+    }
+
+    @Override
+    public void onStop () {
+        super.onStop();
+        audioPlayer.release();
+    }
 }
