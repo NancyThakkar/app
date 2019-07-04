5fec85c56beab130c147a6b365a5d57c28ae5219
jake <jake@mythical.fish>
2019-07-04 10:34:12 +0200

    Add React Native

---
+++ index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { AppRegistry, StyleSheet, Text, View } from 'react-native'
+
+class Priome extends React.Component {
+  render () {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.hello}>Hey! How are you?</Text>
+      </View>
+    )
+  }
+}
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  hello: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  }
+})
+
+AppRegistry.registerComponent('Priome', () => Priome)
